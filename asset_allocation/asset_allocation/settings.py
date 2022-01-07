825dff44a0f3f4acaa6dc2876ae29e2740754ead
AlexGordeev <alexgordeev.vrn@gmail.com>
2022-01-07 13:58:55 +0300

    Add static and media files settings

--- asset_allocation/asset_allocation/settings.py
+++ asset_allocation/asset_allocation/settings.py
@@ -125,7 +125,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = Path(BASE_DIR).joinpath('static')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    Path(BASE_DIR).joinpath('asset_allocation/static'),
+]
+
+
+# Media files
+
+MEDIA_ROOT = Path(BASE_DIR).joinpath('media')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
