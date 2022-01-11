f9a1f76b81eab013be760d25db94c5e1fa7cc43c
AlexGordeev <alexgordeev.vrn@gmail.com>
2022-01-11 22:28:54 +0300

    Add Currency model and CurrencyAdmin model

--- asset_allocation/assets/admin.py
+++ asset_allocation/assets/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from assets import models
+
+
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'iso_code', 'symbol')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'iso_code')
+
+
+admin.site.register(models.Currency, CurrencyAdmin)
--- asset_allocation/assets/models.py
+++ asset_allocation/assets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    """Валюта"""
+    name = models.CharField(max_length=20, verbose_name='Наименование')
+    iso_code = models.CharField(max_length=3, verbose_name='Код ISO 4217')
+    symbol = models.CharField(max_length=1, verbose_name='Символ')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'currency'
+        ordering = ('name',)
+        verbose_name = 'Валюта'
+        verbose_name_plural = 'Валюты'
